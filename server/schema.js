cf6f03e0286fd96e18b6e0d48dbc696a765fde87
Alfred Sta. Iglesia <a.sta.iglesia@gmail.com>
2022-02-14 17:50:12 -0500

    define root query and test with hard data

--- server/schema.js
+++ server/schema.js
@@ -8,8 +8,6 @@ const {
 } = require('graphql');
 
 
-// ======== Hardcoded data for schema testing
-
 
 // ======== Object Types
 // ----- Project Type 
@@ -24,7 +22,7 @@ const ProjectType = new GraphQLObjectType({
     client: {type: GraphQLString},
     company: {type: GraphQLString},
     deliveryDate: {type: GraphQLString},
-    rentalTerm: {type: GraphQLInt},
+    rentalTerm: {type: GraphQLString},
     tag: {type: GraphQLString},
   })
 })
@@ -47,21 +45,30 @@ const ProjectType = new GraphQLObjectType({
 // ----- Root Query 
 // -- each fields property defines an associated nested query
 // -- we can/will create more query types as needed
+// create project queries for each filter type
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
+  // each field is a query definition
   fields: {
-    project: {
-      type: ProjectType
+    // queries all projects -> evaluates to a list
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parentvalue, args){
+        return mockProjects;
+      }
     },
-    // product: {
-    //   type: ProjectType
-    // },
-    // order: {
-    //   type: ProjectType
-    // },
-    // client: {
-    //   type: ProjectType
-    // },
+    // queries a single project, by id
+    project: {
+      type: ProjectType,
+      args: {
+        id: {type: GraphQLString},
+      },
+      resolve(parentvalue, args){
+        for (let i = 0; i < mockProjects.length; i++) {
+          if (mockProjects[i].id == args.id) return mockProjects[i];
+        }
+      }
+    },
   }
 })
 
@@ -69,5 +76,86 @@ const RootQuery = new GraphQLObjectType({
 
 // gql schema accepts a root query as an arg
 module.exports = new GraphQLSchema({
-
+  query: RootQuery,
 })
+
+
+
+
+
+// ======== Test Query with Hard-Coded Data
+// ======== Hardcoded data for schema testing
+// const mockProjects = [
+//   { id: "1",
+//     orderState: 'Draft',
+//     nickname: 'Google Tribeca',
+//     location: 'New York City',
+//     client: 'Alfie S',
+//     company: 'Vaulted Oak',
+//     deliveryDate: new Date(2022, 5, 15),
+//     rentalTerm: '12 months',
+//     tag: 'Breakout Spaces',
+    
+//     // --- properties for additional functionality
+//     // clientProfile: {clientObject: 'to be created'},
+//     // billingInformation: {billingObject: 'to be created'},
+//     // productList: [],
+//   },
+//   { id: "2",
+//     orderState: 'Reserved',
+//     nickname: 'Blackrock Hudson Yards',
+//     location: 'New York City',
+//     client: 'Alfie S',
+//     company: 'Vaulted Oak',
+//     deliveryDate: new Date(2022, 1, 15),
+//     rentalTerm: '12 months',
+//     tag: 'Guest Lounges',
+
+//     // --- properties for additional functionality
+//     // clientProfile: {clientObject: 'to be created'},
+//     // billingInformation: {billingObject: 'to be created'},
+//     // productList: [],
+//   },
+//   { id: "3",
+//     orderState: 'Draft',
+//     nickname: 'Wework Vesey St',
+//     location: 'New York City',
+//     client: 'Fake Client',
+//     company: 'WeWork',
+//     deliveryDate: new Date(2022, 7, 15),
+//     rentalTerm: '12 months',
+//     tag: 'Common Areas',
+
+//     // --- properties for additional functionality
+//     // clientProfile: {clientObject: 'to be created'},
+//     // billingInformation: {billingObject: 'to be created'},
+//     // productList: [],
+//   },
+// ];
+
+
+// const RootQuery = new GraphQLObjectType({
+//   name: 'RootQueryType',
+//   // each field is a query definition
+//   fields: {
+//     // queries all projects -> evaluates to a list
+//     projects: {
+//       type: new GraphQLList(ProjectType),
+//       resolve(parentvalue, args){
+//         return mockProjects;
+//       }
+//     },
+//     // queries a single project, by id
+//     project: {
+//       type: ProjectType,
+//       args: {
+//         id: {type: GraphQLString},
+//       },
+//       resolve(parentvalue, args){
+//         for (let i = 0; i < mockProjects.length; i++) {
+//           if (mockProjects[i].id == args.id) return mockProjects[i];
+//         }
+//       }
+//     },
+//   }
+// })
