e6e327db104a3e5d44d3c62ef8541c5d0cfbb8fb
Alfred Sta. Iglesia <a.sta.iglesia@gmail.com>
2022-02-19 11:40:29 -0500

    add product card to sidebar

--- client/src/features/ManageContent/Sidebar/Sidebar.jsx
+++ client/src/features/ManageContent/Sidebar/Sidebar.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import styles from './Sidebar.module.css';
 
-const Sidebar = () => {
+const Sidebar = props => {
 
   return (
     <div className={styles.sidebar}>
       <div className={styles['sidebar-header']}>
         <h3 className={styles['sidebar-title']}>Product Selection</h3>
         <button className={styles.button}>Term</button>
+        {props.children}
       </div>
     </div>
   )
