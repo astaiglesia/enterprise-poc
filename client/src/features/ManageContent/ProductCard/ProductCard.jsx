45ab7bc1ca827e2ae40ccdc930ee1cd2f9608a22
Alfred Sta. Iglesia <a.sta.iglesia@gmail.com>
2022-02-18 19:16:45 -0500

    scaffold product card

---
+++ client/src/features/ManageContent/ProductCard/ProductCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import styles from './ProductCard.module.css';
+import testImage from "../../../assets/images/green-paint-texture.jpg";
+
+const ProductCard = props =>  {
+  // provide onDoubleClick and DnD functionality
+
+
+  return (
+    <div className={styles['pdp-card']}>
+      <h3 className={styles['pdp-title']}>{props.details.title}</h3>
+      <img className={styles.image} 
+          src={testImage}
+          width="250"
+          height="250"
+          alt={props.details.alt}>
+      </img>
+      <section className={styles['grid-config']}>
+        <div className={styles.gridbox}>Option: {props.details.option}</div>
+        <div className={styles.gridbox}>Color: {props.details.color}</div>
+        <div className={styles.gridbox}>Sku</div>
+        <div className={styles.gridbox}>Supply Chain</div>
+      </section>
+    </div>
+  )
+};
+
+export default ProductCard;
