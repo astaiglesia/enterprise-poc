4794b4577b227c49f7a2c95380df52fe416edb8a
Alfred Sta. Iglesia <a.sta.iglesia@gmail.com>
2022-02-18 19:38:35 -0500

    add styling

--- client/src/features/ManageContent/ProductCard/ProductCard.jsx
+++ client/src/features/ManageContent/ProductCard/ProductCard.jsx
@@ -5,10 +5,10 @@ import testImage from "../../../assets/images/green-paint-texture.jpg";
 
 const ProductCard = props =>  {
   // provide onDoubleClick and DnD functionality
-
+  
 
   return (
-    <div className={styles['pdp-card']}>
+    <div className={styles['pdp-card']} onClick>
       <h3 className={styles['pdp-title']}>{props.details.title}</h3>
       <img className={styles.image} 
           src={testImage}
@@ -17,8 +17,8 @@ const ProductCard = props =>  {
           alt={props.details.alt}>
       </img>
       <section className={styles['grid-config']}>
-        <div className={styles.gridbox}>Option: {props.details.option}</div>
-        <div className={styles.gridbox}>Color: {props.details.color}</div>
+        <div className={styles.gridbox}>{props.details.option}</div>
+        <div className={styles.gridbox}>{props.details.color}</div>
         <div className={styles.gridbox}>Sku</div>
         <div className={styles.gridbox}>Supply Chain</div>
       </section>
