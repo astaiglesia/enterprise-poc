c5033ec5f7a94a0e9e66313f9189e21f7350cf02
Alfred Sta. Iglesia <a.sta.iglesia@gmail.com>
2022-02-20 12:00:07 -0500

    update form to useReducer

--- client/src/features/ProjectsContent/ProjectForm/ProjectForm.jsx
+++ client/src/features/ProjectsContent/ProjectForm/ProjectForm.jsx
@@ -1,91 +1,155 @@
-import React, { useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { createProject } from '../ProjectList/ProjectList/projectListSlice';
 
 import styles from './ProjectForm.module.css';
 
+// Define Reducer Actions
+const CreateProjectReducer = (currentState, action) => {
+  switch (action.type) {
+    case 'SELECT_STATE' : {
+      return {
+        ...currentState,
+        orderState: action.payload,
+      }
+    }
+    case 'INPUT_NICKNAME' : {
+      return {
+        ...currentState,
+        nickname: action.payload,
+      }
+    }
+    case 'BLUR_NICKNAME' : {
+      // create an alert if less than 4 chars
+      return {
+        ...currentState,
+        nicknameIsValid: currentState.nickname.trim().length > 4,
+      }
+    }
+    case 'INPUT_LOCATION' : {
+      return {
+        ...currentState,
+        location: action.payload,
+      }
+    }
+    case 'BLUR_LOCATION' : {
+      // create an alert if less than 4 chars 
+      return {
+        ...currentState,
+        locationIsValid: currentState.location.trim().length > 4,
+      }
+    }
+    case 'INPUT_CLIENT' : {
+      return {
+        ...currentState,
+        client: action.payload,
+      }
+    }
+    case 'BLUR_CLIENT' : {
+      // create an alert if less than 4 chars 
+      return {
+        ...currentState,
+        clientIsValid: currentState.client.trim().length > 4,
+      }
+    }
+    case 'INPUT_COMPANY' : {
+      return {
+        ...currentState,
+        company: action.payload,
+      }
+    }
+    case 'INPUT_DELIVERY' : {
+      return {
+        ...currentState,
+        deliveryDate: action.payload,
+      }
+    }
+    case 'SELECT_TERM' : {
+      return {
+        ...currentState,
+        rentalTerm: action.payload,
+      }
+    }
+    case 'INPUT_TAG' : {
+      return {
+        ...currentState,
+        tag: action.payload,
+      }
+    }
+    default: 
+    return currentState;
+  }
+}
+
+// Use Hooks to Handle Local State of Form Data
 const ProjectForm = () => {
-  // Use Hooks to Handle Local State for Form Data
-  // ### useReducer to simplify form data
-  // ### useEffect to submit form data
-  const [formData, setFormData] = useState({
-    orderState: 'Draft',                       // -- orderState property may be edited in future feature
+  // ---- Defines Initial State & 2-Way Bindings to Form Fields
+  const initialFormState = {
+    orderState: '',                     
     nickname: '',
+    nicknameIsValid: null,
     location: '',
+    locationIsValid: null,
     client: '',
+    clientIsValid: null,
     company: '',
     deliveryDate: '',
     rentalTerm: '',
     tag: '',
-  });
+  };
+  const [formState, dispatchFormData] = useReducer(CreateProjectReducer, initialFormState);
+  
+  // ---- Handles Form Field Updates, Dispatch To Reducer
+  const stateChangeHandler = (e) => {
+    dispatchFormData({type:'SELECT_STATE', payload: e.target.value})
+  };
 
   const nicknameHandler = (e) => {
-    setFormData(currentState => {
-      return {
-        ...currentState,
-        nickname: e.target.value,
-      }
-    })
+    dispatchFormData({type:'INPUT_NICKNAME', payload: e.target.value})
+  };
+  const validNicknameHandler = (e) => {
+    dispatchFormData({type:'BLUR_NICKNAME'})
   };
 
   const locationHandler = (e) => {
-    setFormData(currentState => {
-      return {
-        ...currentState,
-        location: e.target.value,
-      }
-    })
+    dispatchFormData({action: 'INPUT_LOCATION', payload: e.target.value})
+  };
+  const validLocationHandler = (e) => {
+    dispatchFormData({action: 'BLUR_LOCATION'})
   };
 
   const clientHandler = (e) => {
-    setFormData(currentState => {
-      return {
-        ...currentState,
-        client: e.target.value,
-      }
-    })
+    dispatchFormData({action: 'INPUT_CLIENT', payload: e.target.value})
+  };
+  const validClientHandler = (e) => {
+    dispatchFormData({action: 'BLUR_CLIENT'})
   };
 
   const companyHandler = (e) => {
-    setFormData(currentState => {
-      return {
-        ...currentState,
-        company: e.target.value,
-      }
-    })
+    dispatchFormData({action: 'INPUT_COMPANY', payload: e.target.value})
   };
 
   const deliveryDateHandler = (e) => {
-    setFormData(currentState => {
-      return {
-        ...currentState,
-        deliveryDate: e.target.value,
-      }
-    })
+    dispatchFormData({action: 'INPUT_DELIVERY', payload: e.target.value})
   };
 
   const termHandler = (e) => {
-    setFormData(currentState => {
-      return {
-        ...currentState,
-        rentalTerm: e.target.value,
-      }
-    })
+    dispatchFormData({action: 'SELECT_TERM', payload: e.target.value})
   };
 
   const tagHandler = (e) => {
-    setFormData(currentState => {
-      return {
-        ...currentState,
-        tag: e.target.value,
-      }
-    })
+    dispatchFormData({action: 'INPUT_TAG', payload: e.target.value})
   };
 
 
-  // ---- Handle Form Submission 
-  // ### implent async reducer to Post to DB
+  // ---- Handles Form Submission 
+  const [formIsValid, dispatchForm] = useState(false);
+  useEffect(() => {
+    
+  })
+
+  // ### implent async reducer logic to Post to DB
   
   // Dispatches Form State to Redux Store
   const dispatch = useDispatch();
@@ -100,28 +164,42 @@ const ProjectForm = () => {
     dispatch(createProject(newProject));
   }
   
-
   return (
     <form className={styles.form} onSubmit={formSubmissionHandler}>
       <h2>Project Profile</h2>
       
+      <label htmlFor="orderState" >Rental Term</label>
+      <select type="text" 
+              name="orderState"   
+              id="orderState" 
+              value={formState.orderState}
+              onChange={stateChangeHandler} >
+        <option value="Draft" defaultValue>Draft</option>
+        <option value="Reserved">Reserved</option>
+        <option value="onDeposit">onDeposit</option>
+        <option value="Approved">Approved</option>
+        <option value="Subscribed">Subscribed</option>
+        <option value="Closed">Closed</option>
+      </select>
+
       <label htmlFor="nickname" >Project Nickname* </label>
       <input type="text" 
               name="nickname" 
               id="nickname" 
-              value={formData.nickname}
+              value={formState.nickname}
               required={true} 
-              onChange={nicknameHandler} >
+              onChange={nicknameHandler}
+              onBlur={validNicknameHandler} >
       </input>
       
-      {/* refactor to pre-populate based on zip - JS30 */}
       <label htmlFor="location" >Location / City* </label>
       <input type="text" 
               name="location" 
               id="location" 
-              value={formData.location}
+              value={formState.location}
               required={true} 
-              onChange={locationHandler} >
+              onChange={locationHandler} 
+              onBlur={validLocationHandler} >
       </input>
       
       <label htmlFor="client" >Client</label>
@@ -129,15 +207,16 @@ const ProjectForm = () => {
               name="client" 
               id="client" 
               required={true} 
-              value={formData.client}
-              onChange={clientHandler} >
+              value={formState.client}
+              onChange={clientHandler} 
+              onBlur={validClientHandler} >
       </input>
 
       <label htmlFor="company" >Company</label>
       <input type="text" 
               name="company" 
               id="company" 
-              value={formData.company}
+              value={formState.company}
               onChange={companyHandler} >
       </input>
 
@@ -145,7 +224,7 @@ const ProjectForm = () => {
       <input type="date" 
               name="deliveryDate" 
               id="deliveryDate" 
-              value={formData.deliveryDate}
+              value={formState.deliveryDate}
               onChange={deliveryDateHandler}>
       </input>
 
@@ -153,19 +232,19 @@ const ProjectForm = () => {
       <select type="text" 
               name="rentalTerm"   
               id="rentalTerm" 
-              value={formData.rentalTerm}
+              value={formState.rentalTerm}
               onChange={termHandler} >
+        <option value="12" defaultValue>12 Months</option>
         <option value="1">1 Month</option>
         <option value="3">3 Months</option>
         <option value="6">6 Months</option>
-        <option value="12" defaultValue>12 Months</option>
       </select>
 
       <label htmlFor="tag" >Tag</label>
       <input type="text" 
               name="tag" 
               id="tag" 
-              value={formData.tag}
+              value={formState.tag}
               onChange={tagHandler}>
       </input>
 
