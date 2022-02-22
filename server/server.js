c390ef7893a6368573fba2b2eec9350db7c55c01
Alfred Sta. Iglesia <a.sta.iglesia@gmail.com>
2022-02-22 17:22:43 -0500

    add dotenv

--- server/server.js
+++ server/server.js
@@ -2,15 +2,19 @@
 
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
 
 const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./apolloAPI/typeDefs');
 const resolvers = require('./apolloAPI/resolvers');
 
+const mongoose = require('mongoose');
+const MONGO_URI = 'mongodb+srv://astaiglesia:OmegaSupreme@cluster0.rp1x2.mongodb.net/costs?retryWrites=true&w=majority';
+
 
 async function initServer() {
   const app = express();
-  // allow cross-origin access to localhost JSON server
+  // allows cross-origin access 
   app.use(cors());
   
   // serves static assets
@@ -36,6 +40,16 @@ async function initServer() {
   
   const PORT = process.env.PORT || 5000;
   
+  // connect to database
+  mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'costs'
+  })
+    .then(() => console.log(`Connected to Mongo DB on port ${PORT}`))
+    .catch(err => console.log(err));
+
+
   // spin up the server
   app.listen({ port: PORT }, () =>
     console.log(`🚀 Express + GraphQL servers are live and listening on port 5000. \n GraphiQL playground is available at http://localhost:5000${apolloServer.graphqlPath}`)
