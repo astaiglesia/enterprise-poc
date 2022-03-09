2ac25a9ba0637a5d57de8141a660a1a640cdd546
Alfred Sta Iglesia <75825642+astaiglesia@users.noreply.github.com>
2022-03-08 21:27:11 -0500

    add useToggle to managesidebar

--- client/src/features/ManageContent/Gallery/GalleryHeader/GalleryHeader.jsx
+++ client/src/features/ManageContent/Gallery/GalleryHeader/GalleryHeader.jsx
@@ -5,31 +5,22 @@ import styles from './GalleryHeader.module.css';
 
 const GalleryHeader = () => {
   const selectedProject = useSelector(state => state.manageContent.project1);
-  const [ projectData, setProject] = useState(selectedProject);
-  // console.log(projectData);
-  
-  const { _id, nickname, orderState, location, client, company, deliveryDate, rentalTerm, tag } = projectData;
+ 
+  const { _id, nickname, orderState, location, client, company, deliveryDate, rentalTerm, tag } = selectedProject;
   
   return (
     <section className={styles.header}>
-      <div className={styles.manage}>
-        <button className={styles.burger}> - </button>        
-      </div>
-
       <div className={styles['project-title']}>
         <h2 className={styles['header-heading']}>{nickname}</h2>
         <h3 className={styles['header-heading']}>{location}</h3>
       </div> 
-      
       <div className={styles['client-info']}>
         <div>{company}</div>
         <div>{client}</div>
       </div>    
-      
       <div className={styles.state}>
         <h2>{orderState}</h2>
       </div>
-
     </section>
   )
 };
--- client/src/features/ManageContent/ManageContent.jsx
+++ client/src/features/ManageContent/ManageContent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import useToggle from '../../helpers/customHooks/useToggle';
 
 import styles from './ManageContent.module.css';
 
@@ -6,12 +7,17 @@ import Sidebar from './ManageSidebar/ManageSidebar';
 import Gallery from './Gallery/Gallery';
 
 const ManageContent = () => {
-  // const location = useLocation();
-  // console.log(location.state.data);
-
+  const [isToggled, setIsToggled] = useToggle(false);
+  
   return (
     <main className={styles['content-container']}>
-      <Sidebar className={styles.sidebar} />
+      {isToggled && <Sidebar className={styles.sidebar} />}
+      <button 
+        className={styles.burger}
+        onClick={(prevState) => setIsToggled(!prevState)}
+      > 
+        {isToggled ? <h1> - </h1> : <h1> + </h1>} 
+      </button>
       <Gallery />
     </main>
   )
